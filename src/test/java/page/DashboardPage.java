8642960f95bc8876aad4f64b25e2e69de72a1858
Eugen Sabanovskiy <eugensabanovskiy123@gmail.com>
2025-02-28 08:46:46 +0300

    remaded AGAIN

---
+++ src/test/java/page/DashboardPage.java
@@ -0,0 +1,37 @@
+package page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+    private SelenideElement heading = $("[data-test-id=dashboard]");
+    private ElementsCollection cards = $$("div[data-test-id]");
+
+    public DashboardPage() {
+        heading.shouldBe(visible);
+    }
+
+    public TopUpPage clickTopUp(String cardTestId) {
+        $("div[data-test-id='" + cardTestId + "']")
+                .find("button[data-test-id=action-deposit]")
+                .click();
+        return new TopUpPage();
+    }
+
+    public int getBalance(int cardIndex) {
+        String cardText = cards.get(cardIndex).innerText();
+        String[] text = cardText.split(" ");
+        return Integer.parseInt(text[5]);
+    }
+
+    public int getBalance(String cardNumber) {
+        SelenideElement card = cards.findBy(Condition.text(cardNumber));
+        String[] text = card.innerText().split(" ");
+        return Integer.parseInt(text[5]);
+    }
+}
